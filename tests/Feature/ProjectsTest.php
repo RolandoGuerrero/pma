150e2ae1fd3c3aa2254fe5eb399e8d881a4968a3
Rolando Guerrero <rolandoguerrero.devs@gmail.com>
2019-05-28 21:07:22 -0700

    Auth & Create Projects

---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProjectsTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+    
+    /** @test */
+    public function only_authenticated_user_can_create_projects()
+    {        
+        $attributes = factory("App\Models\Project")->raw(['owner_id' => null]);
+
+        $this->post('/projects', $attributes)->assertRedirect('login');
+    }
+
+    /** @test */
+    public function a_user_can_create_a_project()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->actingAs(factory('App\Models\User')->create());
+        
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph
+        ]; 
+
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
+
+        $this->assertDatabaseHas('projects', $attributes);
+
+        $this->get('/projects')->assertSee($attributes['title']);
+    }
+
+    /** @test */
+    public function a_user_can_view_a_projects()
+    {
+        $this->withoutExceptionHandling();
+
+        $project = factory("App\Models\Project")->create();
+    
+        $this->get($project->path())
+            ->assertSee($project->title)
+            ->assertSee($project->description);
+    }
+
+    /** @test */
+    public function a_project_requires_a_title()
+    {
+        $this->actingAs(factory('App\Models\User')->create());
+
+        $attributes = factory("App\Models\Project")->raw(['title' => '']);
+ 
+        $this->post('/projects', $attributes)->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_project_requires_a_description()
+    {
+        $this->actingAs(factory('App\Models\User')->create());
+
+        $attributes = factory("App\Models\Project")->raw(['description' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('description');
+    }
+
+    
+}
